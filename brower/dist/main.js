6424beb9f649efc9e87d7dee33f15366cf8c0d9e
wlghks0119 <wlghks0119@naver.com>
2025-11-07 13:38:48 +0900

    8주차

---
+++ brower/dist/main.js
@@ -0,0 +1,1 @@
+document.querySelector(".form-data"),document.querySelector(".region-name"),document.querySelector(".api-key"),document.querySelector(".errors"),document.querySelector(".loading"),document.querySelector(".result-container"),document.querySelector(".carbon-usage"),document.querySelector(".fossil-fuel"),document.querySelector(".my-region"),document.querySelector(".clear-btn");
